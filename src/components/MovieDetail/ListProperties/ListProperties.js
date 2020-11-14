fc59493dbf158fb55e83a123809d8d3da9a90c25
Adilet312 <adiletm@hotmail.com>
2020-11-13 23:58:24 -0800

    Add updated listProperties component.

--- src/components/MovieDetail/ListProperties/ListProperties.js
+++ src/components/MovieDetail/ListProperties/ListProperties.js
@@ -1,11 +1,9 @@
 import React,{ useState, useEffect, Fragment}  from 'react';
 import { IoIosArrowForward, IoIosArrowDown} from 'react-icons/io';
 import './ListProperties.css'
-const ListProperties = ({image, name, created_by, description }) => {
-const [ showDescription, setShowDescription ] = useState(true);
+const ListProperties = ({image,  created_by, description }) => {
 const [arrows, setArrows ] = useState({
   imageToggle:true,
-  titleToggle:true,
   authorToggle:true,
   descriptionToggle:true
 });
@@ -14,11 +12,29 @@ const [arrows, setArrows ] = useState({
       <ul className = 'list-properies'>
         <li className = 'description-container'>
           <h3>Description</h3>
+          <span className = 'arrow-sign' onClick = {(e) => { e.currentTarget.parentNode.classList.toggle('description-container');setArrows({...arrows, descriptionToggle:!arrows.descriptionToggle})}}>
+              {arrows.descriptionToggle ? (<IoIosArrowForward className = 'arrowForward' style = {{fontSize:"20px"}}/>) : (<IoIosArrowDown className = 'arrowDown' style = {{fontSize:"20px"}}/>)}
+          </span>
+          <div className = 'add-description' style = {{fontFamily:"Merienda One"}}>
+            {description}
+          </div>
+        </li>
+        <li className = 'description-container'>
+          <h3>Author</h3>
+          <span className = 'arrow-sign' onClick = {(e) => { e.currentTarget.parentNode.classList.toggle('description-container');setArrows({...arrows, authorToggle:!arrows.authorToggle})}}>
+              {arrows.authorToggle? (<IoIosArrowForward className = 'arrowForward' style = {{fontSize:"20px"}}/>) : (<IoIosArrowDown className = 'arrowDown' style = {{fontSize:"20px"}}/>)}
+          </span>
+          <div className = 'add-description' style = {{fontFamily:"Merienda One"}}>
+            {created_by}
+          </div>
+        </li>
+        <li className = 'description-container'>
+          <h3>Image</h3>
           <span className = 'arrow-sign' onClick = {(e) => { e.currentTarget.parentNode.classList.toggle('description-container');setArrows({...arrows, imageToggle:!arrows.imageToggle})}}>
               {arrows.imageToggle ? (<IoIosArrowForward className = 'arrowForward' style = {{fontSize:"20px"}}/>) : (<IoIosArrowDown className = 'arrowDown' style = {{fontSize:"20px"}}/>)}
           </span>
           <div className = 'add-description' style = {{fontFamily:"Merienda One"}}>
-            {description}
+            <img src = {image}/>
           </div>
         </li>
       </ul>
